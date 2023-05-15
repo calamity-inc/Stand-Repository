a3c32426b02fcb56e6cbd15154eccd844d77904d
Tyler Chamberlain <hexarobi@gmail.com>
2023-05-15 13:30:59 -0500

    Update Constructor to 0.37.2r

--- data/packages.php
+++ data/packages.php
@@ -502,7 +502,8 @@ $packages = [
 	"lua/Constructor" => [
 		"priority" => 2,
 		"author" => "hexarobi",
-		"version" => "0.37r",
+		"description" => "Load and edit custom map, vehicle and skin files in JSON, XML or INI format.",
+		"version" => "0.37.2r",
 		"depends" => [
 			"lua/natives-1672190175",
 			"lua/iniparser",
@@ -511,8 +512,8 @@ $packages = [
 			"lua/quaternionLib",
 		],
 		"files" => [
-			"Constructor.lua" => "raw.githubusercontent.com/hexarobi/stand-lua-constructor/dc8c2e443c593c3029b17956ab17c453da554cad/Constructor.lua",
-			"lib/auto-updater.lua" => "raw.githubusercontent.com/hexarobi/stand-lua-constructor/dc8c2e443c593c3029b17956ab17c453da554cad/lib/auto-updater.lua",
+			"Constructor.lua" => "raw.githubusercontent.com/hexarobi/stand-lua-constructor/58d3c90dbb0cd3cd36f8c93d55276d7e96dae828/Constructor.lua",
+			"lib/auto-updater.lua" => "raw.githubusercontent.com/hexarobi/stand-lua-constructor/a4a3dc7973b352b79dcf44e5056becc67a5af20a/lib/auto-updater.lua",
 			"lib/constructor/constants.lua" => "raw.githubusercontent.com/hexarobi/stand-lua-constructor/dc8c2e443c593c3029b17956ab17c453da554cad/lib/constructor/constants.lua",
 			"lib/constructor/constructor_lib.lua" => "raw.githubusercontent.com/hexarobi/stand-lua-constructor/dc8c2e443c593c3029b17956ab17c453da554cad/lib/constructor/constructor_lib.lua",
 			"lib/constructor/convertors.lua" => "raw.githubusercontent.com/hexarobi/stand-lua-constructor/dc8c2e443c593c3029b17956ab17c453da554cad/lib/constructor/convertors.lua",
