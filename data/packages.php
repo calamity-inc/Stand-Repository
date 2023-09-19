4474935131b5854d63addc0355e62b63d02a9763
Sainan <sainan@calamity.gg>
2023-09-19 20:48:41 +0200

    Cleanup

--- data/packages.php
+++ data/packages.php
@@ -178,16 +178,11 @@ $packages = [
 		"files" => [
 			"Dolos.pluto" => "raw.githubusercontent.com/thebitwise/dolos/2.0.1/Dolos.pluto",
 		],
-		"resources_version" => "2.0.1",
 		"resources" => [
 			"resources/dolos/attention.wav" => "github.com/thebitwise/dolos/raw/2.0.1/resources/dolos/attention.wav",
 			"resources/dolos/fail.wav" => "github.com/thebitwise/dolos/raw/2.0.1/resources/dolos/fail.wav",
 			"resources/dolos/processing.wav" => "github.com/thebitwise/dolos/raw/2.0.1/resources/dolos/processing.wav",
 			"resources/dolos/success.wav" => "github.com/thebitwise/dolos/raw/2.0.1/resources/dolos/success.wav",
-		],
-
-		"trash" => [
-			"resource/dolos",
 		],
 	],
 	"lua/iniparser" => [
