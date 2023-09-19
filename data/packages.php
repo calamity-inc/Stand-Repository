0ca179cd53347d19359a5d25ab38fd31915223f0
Sainan <sainan@calamity.gg>
2023-09-19 20:19:35 +0200

    Update LanceScript to forever-gh-1.0.1

--- data/packages.php
+++ data/packages.php
@@ -154,9 +154,9 @@ $packages = [
 		"depends" => [
 			"lua/natives-2944b",
 		],
-		"version" => "forever-gh-1.0.0",
+		"version" => "forever-gh-1.0.1",
 		"files" => [
-			"LanceScript.pluto" => "raw.githubusercontent.com/thebitwise/LanceScript-Forever/1.0.0/LanceScript Forever.pluto",
+			"LanceScript.pluto" => "raw.githubusercontent.com/thebitwise/LanceScript-Forever/1.0.1/LanceScriptForever.pluto",
 		],
 		"resources_version" => "forever-gh-1.0.0",
 		"resources" => [
