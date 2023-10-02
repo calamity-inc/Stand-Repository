1f60f4af0b6033f00aed9fed508b2cd56a0ae7d8
Prisuhm <109115519+Prisuhm@users.noreply.github.com>
2023-10-02 06:20:12 -0700

    JinxScript Update 0.45

--- data/packages.php
+++ data/packages.php
@@ -557,12 +557,13 @@ $packages = [
 	"lua/JinxScript" => [
 		"priority" => PRIORITY_BLOATWARE,
 		"author" => "Prisuhm",
+		"description" => "A feature rich script designed to take aim at modders with a handful of unique trolling features and a plethora of useful modder detections.",
 		"depends" => [
 			"lua/natives-2944a"
 		],
-		"version" => "0.44",
+		"version" => "0.45",
 		"files" => [
-			"JinxScript.pluto" => "raw.githubusercontent.com/Prisuhm/JinxScript/7d863fa3c7be94b8778b4eb6ded1d05ab99d5f4b/JinxScript.pluto",
+			"JinxScript.pluto" => "raw.githubusercontent.com/Prisuhm/JinxScript/23e8f93f16fd2b723a11a8109f08c695399d0ca5/JinxScript.pluto",
 		],
 	],
 	"lua/AcjokerScript" => [
