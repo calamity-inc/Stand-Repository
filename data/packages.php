2c7b639dbdb229f314d01fa12226fc4669c2822f
Tyler Chamberlain <hexarobi@gmail.com>
2024-03-02 20:28:13 -0600

    fix comma

--- data/packages.php
+++ data/packages.php
@@ -531,10 +531,10 @@ $packages = [
 		"version" => "1.6-1",
 		"files" => [
 			"lib/xml2lua.lua" => "raw.githubusercontent.com/manoelcampos/xml2lua/v1.6-1/xml2lua.lua",
-			"lib/XmlParser.lua" => "raw.githubusercontent.com/manoelcampos/xml2lua/v1.6-1/XmlParser.lua"
-			"lib/xmlhandler/tree.lua" => "raw.githubusercontent.com/manoelcampos/xml2lua/v1.6-1/xmlhandler/tree.lua"
-			"lib/xmlhandler/dom.lua" => "raw.githubusercontent.com/manoelcampos/xml2lua/v1.6-1/xmlhandler/dom.lua"
-			"lib/xmlhandler/print.lua" => "raw.githubusercontent.com/manoelcampos/xml2lua/v1.6-1/xmlhandler/print.lua"
+			"lib/XmlParser.lua" => "raw.githubusercontent.com/manoelcampos/xml2lua/v1.6-1/XmlParser.lua",
+			"lib/xmlhandler/tree.lua" => "raw.githubusercontent.com/manoelcampos/xml2lua/v1.6-1/xmlhandler/tree.lua",
+			"lib/xmlhandler/dom.lua" => "raw.githubusercontent.com/manoelcampos/xml2lua/v1.6-1/xmlhandler/dom.lua",
+			"lib/xmlhandler/print.lua" => "raw.githubusercontent.com/manoelcampos/xml2lua/v1.6-1/xmlhandler/print.lua",
 		]
 	],
 	"lua/inspect.lua" => [
