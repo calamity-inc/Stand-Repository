102657483599f464e30ed4e68aa110de4a467af6
Prisuhm <109115519+Prisuhm@users.noreply.github.com>
2024-04-24 00:40:52 -0700

    JinxScript 0.50.3 (#133)
    some bug fixes and improvements

--- data/packages.php
+++ data/packages.php
@@ -439,9 +439,9 @@ $packages = [
 		"depends" => [
 			"lua/natives-3095a"
 		],
-		"version" => "0.50.2",
+		"version" => "0.50.3",
 		"files" => [
-			"JinxScript.pluto" => "raw.githubusercontent.com/Prisuhm/JinxScript/13b34c99ef4506a61cf5a5508ea33a0c83f2a104/JinxScript.pluto",
+			"JinxScript.pluto" => "raw.githubusercontent.com/Prisuhm/JinxScript/c3595ff7fd0044f998814fba941d7b15a90dc3db/JinxScript.pluto",
 		],
 	],
 	"lua/AcjokerScript" => [
